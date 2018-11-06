4e17da3af7728bcd9f3b048e801dab939bf51df6
hadoop <essentialva@163.com>
2018-11-06 19:09:28 +0800

    mapreduce

--- MapReduce/src/TokenizerMapper.java
+++ MapReduce/src/TokenizerMapper.java
@@ -5,19 +5,20 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
-class TokenizerMapper extends Mapper<Object,Text, Text, IntWritable>{
+public class TokenizerMapper extends Mapper<Object, Text,Text, IntWritable> {
 
     private static final IntWritable one = new IntWritable(1);
     private Text word = new Text();
-    public TokenizerMapper(){
+
+    public TokenizerMapper() {
 
     }
-    public void map(Object key,Text value,Mapper<Object,Text,Text,IntWritable>.Context context) throws IOException,InterruptedException{
+
+    public void map(Object key,Text value,Mapper<Object,Text,Text,IntWritable>.Context context)throws IOException,InterruptedException{
         StringTokenizer itr = new StringTokenizer(value.toString());
         while(itr.hasMoreTokens()){
-            this.word.set(itr.nextToken());
-            context.write(this.word,one);
+            word.set(itr.nextToken());
+            context.write(word,one);
         }
     }
-
 }
--- MapReduce/src/WordCount.java
+++ MapReduce/src/WordCount.java
@@ -2,7 +2,6 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -13,6 +12,7 @@ public class WordCount {
 
     //MapReduce程序：统计词频
     public static void main(String args[]) throws Exception {
+
         Configuration conf = new Configuration();
         String[] otherArgs = (new GenericOptionsParser(conf,args)).getRemainingArgs();
         if(otherArgs.length<2){
